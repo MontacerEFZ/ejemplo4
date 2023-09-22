ce147961f625fc5ea5e69449402bdd4a3776b733
montacer <montacer.elfazazielfazazi@cieep.com>
2023-09-22 10:37:41 +0200

    good

--- app/src/main/java/montacer/elfazazi/ejemplo4/DesencriptarActivity.java
+++ app/src/main/java/montacer/elfazazi/ejemplo4/DesencriptarActivity.java
@@ -2,6 +2,7 @@ package montacer.elfazazi.ejemplo4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -11,6 +12,13 @@ public class DesencriptarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desencriptar);
-        Toast.makeText(this, "he llegado", Toast.LENGTH_SHORT).show();
+
+        //sacar la informacion que llega
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        if (bundle != null){
+            String password = bundle.getString("PASS");
+            Toast.makeText(this, password, Toast.LENGTH_SHORT).show();
+        }
     }
 }
